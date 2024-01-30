d3131dff35af547f3f674f349f5074da12adc9e0
owenbmcc <oroberts@bmcc.cuny.edu>
2024-01-29 23:55:35 -0500

    spring 2024, godot 4 updates

--- Scripts/Metrics.gd
+++
@@ -1,7 +0,0 @@
-extends Control
-
-var apple_count = 0
-
-func _on_Apple_collected():
-	apple_count = apple_count + 1
-	$Label.text = String(apple_count)
--- Scripts/Portal.gd
+++
@@ -1,9 +0,0 @@
-extends Area
-
-# what level to load next
-export (String, FILE, '*.tscn') var load_level
-
-# when player has entered
-func _on_Portal_body_entered(_body):
-	if ResourceLoader.exists(load_level):
-		get_tree().change_scene(load_level)
