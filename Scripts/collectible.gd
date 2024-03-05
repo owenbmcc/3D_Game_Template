ec606c3e275d01d0ef3b3ef193c322b2831f48e6
owenbmcc <oroberts@bmcc.cuny.edu>
2024-03-05 15:04:09 -0500

    adds collectible

---
+++ Scripts/collectible.gd
@@ -0,0 +1,10 @@
+extends Area3D
+
+var is_collected = false
+
+func _on_body_entered(_body):
+	# when player enters
+	# increment apple count by 1
+	global.apple_count = global.apple_count + 1
+	print("apple count", global.apple_count)
+	queue_free() # remove object from scene
---
+++ Scripts/global.gd
@@ -0,0 +1,5 @@
+extends Node
+
+# counter for apple collectible
+var apple_count = 0
+# reference with global.apple_count
