313837787ffa1e542b582eaf3f4cf75c8550e88b
owenbmcc <oroberts@bmcc.cuny.edu>
2024-03-26 17:17:35 -0400

    pickup fixes

--- Scripts/pickup.gd
+++ Scripts/pickup.gd
@@ -14,16 +14,26 @@ signal update_console
 func _physics_process(_delta):
 	# check that we have an object picked up
 	if pickup_object and is_picked:
+		
 		# get the distance between object and hand
 		var obj_pos = pickup_object.global_transform.origin
 		var hand_pos = hand.global_transform.origin
+		
+		if pickup_object.freeze:
+			pickup_object.global_transform.origin = hand_pos
+			return
+		
 		# get the direction of that distance
 		var dir = (hand_pos - obj_pos).normalized() * pull_speed
 		# move the object in that direction
 		pickup_object.set_linear_velocity(dir)
+		var distance = hand_pos - obj_pos
+		if (distance.length_squared() < 0.1):
+			pickup_object.freeze = true
 
 # detect user input
 func _unhandled_input(_event):
+	print('pick 1', pickup_object)
 	# if user hits "f" or pickup button
 	if Input.is_action_just_pressed("pickup"):
 		emit_signal("update_console", "")
@@ -32,20 +42,26 @@ func _unhandled_input(_event):
 			# if object already picked up
 			if is_picked:
 				# drop object
+				if pickup_object.freeze:
+					pickup_object.freeze = false
 				pickup_object = null
 				is_picked = false
+				
 			else:
 				# pick up object
 				is_picked = true
 				$"../../Pickup".play()
 	
 	# detect user clicked throw
-	if Input.is_action_just_pressed("throw"):
+	if Input.is_action_just_pressed("throw") and pickup_object:
+		if pickup_object.freeze:
+			pickup_object.freeze = false
 		# get direction of camera
 		var dir = -camera.global_transform.basis.z.normalized()
 		# add an upward direction
 		dir = dir + Vector3(0, 1, 0)
 		# apply force in direction with throw_speed
+		
 		pickup_object.apply_impulse(dir * throw_speed)
 		# remove object
 		pickup_object = null
